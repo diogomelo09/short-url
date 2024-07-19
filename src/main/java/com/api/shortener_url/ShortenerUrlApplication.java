3c12282d106e48ccfc3aef88e84be52010939ea8
Diogo Melo <104102585+diogomelo09@users.noreply.github.com>
2024-07-19 19:51:43 -0300

    FormatSource

--- src/main/java/com/api/shortener_url/ShortenerUrlApplication.java
+++ src/main/java/com/api/shortener_url/ShortenerUrlApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShortenerUrlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShortenerUrlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenerUrlApplication.class, args);
+    }
 
 }
--- src/main/java/com/api/shortener_url/controller/dto/UrlRequest.java
+++ src/main/java/com/api/shortener_url/controller/dto/UrlRequest.java
@@ -2,5 +2,3 @@ package com.api.shortener_url.controller.dto;
 
 public record UrlRequest(String url) {
 }
-
-
